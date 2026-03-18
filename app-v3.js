a4e61af88be92b8a19acba99af3ef582cd73725f
nielsendesignpictures-hub <nielsendesignpictures@gmail.com>
2026-03-18 19:17:59 +0100

    Update app-v3.js

--- app-v3.js
+++ app-v3.js
@@ -1,7 +1,7 @@
 // ==========================
 // KONFIG (Google Apps Script webhook)
 // ==========================
-const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyWds-PHayXHll-vvpUwgs2vCU6KqFkgd9Sceg63fB1xM1_o7ZRH3jZrA778ZsxRaOMfw/exec";"; 
+const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyWds-PHayXHll-vvpUwgs2vCU6KqFkgd9Sceg63fB1xM1_o7ZRH3jZrA778ZsxRaOMfw/exec";
 const WEBHOOK_SECRET = "Kaiser-StdKontrol-20260306-a8k3m9q2x1";
 const RATING_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyWds-PHayXHll-vvpUwgs2vCU6KqFkgd9Sceg63fB1xM1_o7ZRH3jZrA778ZsxRaOMfw/exec";
 
